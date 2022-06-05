0f53e66e8f70a4febcf5cb0bf134e8b2ccaeef31
gadarobale <zmoti1234@gmail.com>
2022-06-04 22:22:09 -0700

    4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
  int main(void)
 {
-	/**
-	 *it is comment
-	 */
-	puts("programming is like building a multilingual puzzle");
+	
+	puts("\"programming is like building a multilingual puzzle");
 	return(0);
 }
