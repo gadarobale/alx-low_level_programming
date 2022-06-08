463199234c4b830877f61d4539bbd69a76356608
gadarobale <zmoti1234@gmail.com>
2022-06-08 15:55:57 -0700

    good to go

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,15 @@
  * main - display 'a-z'
  * Return: Always 0.
  */
-int main(viod)
+int main(void)
 {
+	int i;
 	char ch;
-	for(int i = 0 ; i < 10 ; i++)
+	for(i = 0 ; i < 10 ; i++)
 	{
-		m
         	for(ch = 'a'; ch <= 'z'; ch++)
         	{
-		putchar(ch);
+	        	putchar(ch);
 		}
 	}
       	return (0);
