bf99a7322ed3d4778de90dc1e6d8e93681ade5fa
gadarobale <zmoti1234@gmail.com>
2022-06-08 16:53:21 -0700

    good to go

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /*
- * print_alphabet - display 'a-z'
+ * print_alphabet - print out lower case alphabet
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char ch;
 
-	for(ch= 'a'; ch <='z'; ch++)
+	for(ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * main - display 'a-z'
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	char ch;
