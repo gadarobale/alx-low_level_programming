fc03ca76e2092bff67f7c4ecf78ed46b9a499c9d
gadarobale <zmoti1234@gmail.com>
2022-06-08 15:35:19 -0700

    1. _.. | 2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char ch;
+    {
+    for(ch = 'A' ; ch <= 'Z' ; ch++)
+{
+     putchar(ch);
+     return(0);
+}
+}
+#include "main.h"
+/*
+ * main - display 'a-z'
+ * Return: Always 0.
+ */
+int main(viod)
+{
+	char ch;
+	for(ch= 'a'; ch <='z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * main - display 'a-z'
+ * Return: Always 0.
+ */
+int main(viod)
+{
+	char ch;
+	for(int i = 0 ; i < 10 ; i++)
+	{
+		m
+        	for(ch = 'a'; ch <= 'z'; ch++)
+        	{
+		putchar(ch);
+		}
+	}
+      	return (0);
+}
