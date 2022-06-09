1d5fcb8386200c714e3bbc7a41d442dd834eaa8b
gadarobale <zmoti1234@gmail.com>
2022-06-08 19:32:57 -0700

    good to go

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * main -  function that computes the absolute value of an integer.
+ * @n: the main fuction take argument
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+int _abc(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * print_last_digit: -  prints the last digit of a number.
+ *@n: the main fuction take argument.
+ * Returns n otherwise
+ */
+int pint_last_digit(int n)
+{
+	int i;
+	if (n < 0)
+	_puchar (n % 10);
+	return (x);
+}
