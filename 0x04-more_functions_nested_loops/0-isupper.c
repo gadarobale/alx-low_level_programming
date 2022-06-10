ea220bc3109c5c89884fba3488f2464b1c3c5060
gadarobale <zmoti1234@gmail.com>
2022-06-10 13:29:59 -0700

    good to go

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(char c);
+{
+	char c;
+	char up_c;
+
+	up_c = toupper(c);
+	if( c == up_c)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
